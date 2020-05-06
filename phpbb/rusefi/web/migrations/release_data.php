8c60f226d8fa0076893743e8f08a52c58be0c52f
rusefi <rusefillc@gmail.com>
2020-05-05 21:15:26 -0400

    5 bytes of progress

--- phpbb/rusefi/web/migrations/release_data.php
+++ phpbb/rusefi/web/migrations/release_data.php
@@ -27,4 +27,35 @@ class release_data extends \phpbb\db\migration\migration
 			)),
 		);
 	}
+
+
+	/**
+   	 * Linked Lists table initialization
+   	 */
+   	public function update_schema()
+   	{
+   		return array(
+   			'add_tables' => array(
+   				$this->table_prefix . 'rusefi_tokens' => array(
+   					'COLUMNS'     => array(
+   						'user_id'        => array('UINT', 0),
+   						'token'          => array('VCHAR:129', ''),
+   						'created_at'     => array('TIMESTAMP', 0),
+   					),
+   					'PRIMARY_KEY' => array('user_id'),
+   				),
+   			),
+   		);
+   	}
+
+   	public function revert_schema()
+   	{
+   		return array(
+   			'drop_tables' => array(
+   				$this->table_prefix . 'rusefi_tokens',
+  				$this->table_prefix . 'rusefi_vehicles',
+   			),
+   		);
+   	}
+
 }
